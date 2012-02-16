3f2cd8ee0e3eb2763048515f0c8bbbafa2b79838
Claudio Riva <clody69@gmail.com>
2012-02-16 13:49:53 +0200

    Added file and device examples

---
+++ fileAPITests/server.rb
@@ -0,0 +1,60 @@
+require 'sinatra'
+require 'json'
+require 'haml'
+require 'base64'
+
+$articles = [{ :title => "Welcome", 
+               :content => "My first post",
+               :name => "Hello",
+               :email => "hello@blog.com",
+               :timestamp => "1.1.2012 10:20:30",
+               :id => 0}]
+
+def timestamp
+  Time.now.strftime("%d.%m.%Y %H:%M:%S")
+end
+
+get '/' do 
+  #Renders the haml template index.html.haml
+  #with the default layout layout.html.haml
+  haml :index
+end
+
+post '/uploadText' do
+  raw = request.env["rack.input"].read
+  puts "Received data:" + raw
+end
+
+post '/uploadFile' do
+  raw = request.env["rack.input"].read
+  #raw = request.body.read
+  puts "Received data size:" + raw.length.to_s
+  File.open("public/"+request.env["HTTP_X_FILE_NAME"], "w") do |f| 
+    #f.puts Base64.decode64(raw);   
+    f.puts raw;    
+  end 
+end
+
+post '/uploadFormData' do
+  raw = request.env["rack.input"].read
+  #raw = request.body.read
+  #puts raw
+  puts params
+  params.each do |k,v|
+    File.open("public/"+v[:filename], "w") do |f| 
+      f.puts v[:tempfile].read;    
+    end 
+  end
+  redirect to('/')
+end
+
+post '/uploadBase64' do
+  raw = request.env["rack.input"].read.split(',')[1]
+  puts request.env["HTTP_X_FILE_NAME"]
+  
+  File.open("public/"+request.env["HTTP_X_FILE_NAME"], "w") do |f| 
+    f.puts Base64.decode64(raw);   
+  end 
+end
+
+
