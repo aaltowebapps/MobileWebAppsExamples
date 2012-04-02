6c1becbd95d1439074ab56905728d5dab525bcee
Antti Vuorela <antti.vuorela@iki.fi>
2012-04-02 15:59:33 +0300

    Added support for uploads to the lab 4 template server

--- lab4Template/server.rb
+++ lab4Template/server.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'base64'
 
 set :public_folder, settings.root
 
@@ -6,3 +7,11 @@ get '/' do
   send_file 'index.html'
 end
 
+post '/uploadBase64' do
+  raw = request.env["rack.input"].read.split(',')[1]
+  puts request.env["HTTP_X_FILE_NAME"]
+  
+  File.open("public/"+request.env["HTTP_X_FILE_NAME"], "w") do |f| 
+    f.puts Base64.decode64(raw);   
+  end 
+end
